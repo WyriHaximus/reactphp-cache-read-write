1319fb997b998e23c3ba991da7d9ece8903bf409
Cees-Jan Kiewiet <ceesjank@gmail.com>
2022-08-11 13:22:56 +0200

    Update to PHP 8.1

--- src/ReadWrite.php
+++ src/ReadWrite.php
@@ -1,76 +1,108 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace WyriHaximus\React\Cache;
 
 use React\Cache\CacheInterface;
-use React\Promise\PromiseInterface;
 
 final class ReadWrite implements CacheInterface
 {
-    /** @var CacheInterface */
-    private $read;
-
-    /** @var CacheInterface */
-    private $write;
-
-    public function __construct(CacheInterface $read, CacheInterface $write)
-    {
-        $this->read = $read;
-        $this->write = $write;
+    public function __construct(
+        private readonly CacheInterface $read,
+        private readonly CacheInterface $write,
+    ) {
     }
 
     /**
-     * @param  string           $key
-     * @param  null             $default
-     * @return PromiseInterface
+     * @inheritDoc
+     * @phpstan-ignore-next-line
      */
     public function get($key, $default = null)
     {
+        /**
+         * @psalm-suppress TooManyTemplateParams
+         */
         return $this->read->get($key, $default);
     }
 
     /**
-     * @param  string           $key
-     * @param  mixed            $value
-     * @param  null             $ttl
-     * @return PromiseInterface
+     * @inheritDoc
+     * @phpstan-ignore-next-line
      */
     public function set($key, $value, $ttl = null)
     {
+        /**
+         * @psalm-suppress TooManyTemplateParams
+         */
         return $this->write->set($key, $value, $ttl);
     }
 
     /**
-     * @param  string           $key
-     * @return PromiseInterface
+     * @inheritDoc
      */
     public function delete($key)
     {
+        /**
+         * @psalm-suppress TooManyTemplateParams
+         */
         return $this->write->delete($key);
     }
 
+    /**
+     * @inheritDoc
+     * @phpstan-ignore-next-line
+     */
     public function getMultiple(array $keys, $default = null)
     {
+        /**
+         * @psalm-suppress TooManyTemplateParams
+         */
         return $this->read->getMultiple($keys, $default);
     }
 
+    /**
+     * @inheritDoc
+     * @phpstan-ignore-next-line
+     */
     public function setMultiple(array $values, $ttl = null)
     {
+        /**
+         * @psalm-suppress TooManyTemplateParams
+         */
         return $this->write->setMultiple($values, $ttl);
     }
 
+    /**
+     * @inheritDoc
+     */
     public function deleteMultiple(array $keys)
     {
+        /**
+         * @psalm-suppress TooManyTemplateParams
+         */
         return $this->write->deleteMultiple($keys);
     }
 
+    /**
+     * @inheritDoc
+     */
     public function clear()
     {
+        /**
+         * @psalm-suppress TooManyTemplateParams
+         */
         return $this->write->clear();
     }
 
+    /**
+     * @inheritDoc
+     */
     public function has($key)
     {
+        /**
+         * @psalm-suppress TooManyTemplateParams
+         */
         return $this->read->has($key);
     }
 }
